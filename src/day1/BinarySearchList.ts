53e906e31358639487ae29466cd55c2ee107b304
daltondiaz <d_tecnologia@yahoo.com.br>
2022-09-15 23:36:54 -0300

    feat: make Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let lo = 0
+    let hi = haystack.length
+    do {
+        let m = Math.floor(lo + (hi - lo) / 2)
+        let v = haystack[m]
+        if (needle === v ) {
+            return true
+        } else if ( needle > v) {
+           lo = m + 1 
+        } else {
+           hi = m
+        }
+    } while(lo < hi)
+    return false
 }
